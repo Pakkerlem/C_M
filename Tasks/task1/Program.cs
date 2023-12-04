42143ed8f0f9b33c45963180f95625c43058cebc
Pakker <gfabinam@gmail.com>
2023-12-04 16:21:59 +0300

    4_h

--- Tasks/task1/Program.cs
+++ Tasks/task1/Program.cs
@@ -1,17 +1,17 @@
 ﻿// Задача 1: Задайте одномерный массив из 10 целых чисел от 1 до 100. Найдите количество элементов массива, значения которых лежат в отрезке [20,90].
 
-int [] array = new int[10];
+int[] array = new int[10];
 for (int i = 0; i < array.Length; i++)
 {
-array[i] = new Random().Next(1, 101);
-Console.Write(array[i] + " ");
+    array[i] = new Random().Next(1, 101);
+    Console.Write(array[i] + " ");
 }
 int R = 0;
 foreach (int element in array)
 {
-if (element >= 20 && element <= 90)
-{
-    R++;
-}
+    if (element >= 20 && element <= 90)
+    {
+        R++;
+    }
 }
 Console.WriteLine("Количество элементов= " + R);
---
+++ Tasks/task2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 2: Задайте массив на 10 целых чисел. Напишите программу, которая определяет количество чётных чисел в массиве.
+
+int[] array = new int[10];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(1, 101);
+    Console.Write(array[i] + " ");
+}
+int R = 0;
+foreach (int element in array)
+{
+    if (element % 2 == 0)
+    {
+        R++;
+    }
+}
+Console.WriteLine("Количество четных элементов= " + R);
